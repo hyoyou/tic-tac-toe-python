f8e28063a770f20884689ba2e59b29971ca9ef49
Heather You <heather.fswd@gmail.com>
2018-11-28 14:44:48 -0500

    Add setUp() to game test

--- test/test_game.py
+++ test/test_game.py
@@ -5,11 +5,15 @@ from tictactoe.player import Player
 from tictactoe.board import Board
 
 class TestGame(unittest.TestCase):
+    def setUp(self):
+        self.game = Game()
+
     def testGameBoard(self):
-        game = Game()
-        self.assertEqual(game._board, [" " for i in range(9)])
+        self.assertEqual(self.game._board._board, [" " for i in range(9)])
     
     def testGamePlayers(self):
-        game = Game()
-        self.assertEqual(game._player1._symbol, "X")
-        self.assertEqual(game._player2._symbol, "O")
+        self.assertEqual(self.game._player1._symbol, "X")
+        self.assertEqual(self.game._player2._symbol, "O")
+
+        
+    
