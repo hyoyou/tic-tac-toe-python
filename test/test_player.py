2a6bde0100f6fa3b129bdb9fe0df5600232074f9
Heather You <heather.fswd@gmail.com>
2018-11-28 11:24:10 -0500

    Add Player.move() and test

--- test/test_player.py
+++ test/test_player.py
@@ -1,7 +1,14 @@
 import unittest
+from unittest.mock import patch
 from tictactoe.player import Player
+from tictactoe.board import Board
 
 class PlayerTest(unittest.TestCase):
     def testPlayerSymbol(self):
         player = Player("X")
         self.assertEqual(player._symbol, "X")
+
+    @patch('tictactoe.player.Player.move', return_value = 1)
+    def testPlayerMove(self, player_move):
+        player = Player("X")
+        self.assertEqual(player.move(Board()), 1)
--- tictactoe/player.py
+++ tictactoe/player.py
@@ -1,3 +1,6 @@
 class Player:
     def __init__(self, symbol):
         self._symbol = symbol
+
+    def move(self, board):
+        player_move = input("Please make a move: ")
