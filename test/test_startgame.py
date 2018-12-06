a4ed37364a06d341f09235fb898571272f37a26c
Heather You <heather.fswd@gmail.com>
2018-12-06 16:52:43 -0500

    Import AIPlayer class into StartGame

--- test/test_startgame.py
+++ test/test_startgame.py
@@ -6,6 +6,7 @@ from tictactoe.game import Game
 from test.mock_cli_input import MockCLIInput
 from tictactoe.cli_output import CLIOutput
 from tictactoe.player import Player
+from tictactoe.ai_player import AIPlayer
 from startgame import StartGame
 
 class StartGameTest(unittest.TestCase):
