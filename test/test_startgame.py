8d24164f89b9cb8e131b3a235e43e9920fb433bf
Heather You <heather.fswd@gmail.com>
2018-12-07 11:45:00 -0500

    Comment out startgame test, remove input_getter from AI constructor

--- test/test_startgame.py
+++ test/test_startgame.py
@@ -20,13 +20,13 @@ class StartGameTest(unittest.TestCase):
         expected_result = "Welcome to Tic Tac Toe\n"
         self.assertEqual(result, expected_result, msg='\nRetrieved:\n{0} \nExpected:\n{1}'.format(result, expected_result))
 
-    @patch("startgame.StartGame.display_menu", return_value="2")
-    def testChooseGameHumanVsHuman(self, mock_stdin):
-        # want to write a test that checks that players are objects of the Player class
-        # result = type(---._player1) // Cannot test local variable
-        # expected_result = Player
+    # @patch("startgame.StartGame.display_menu", return_value="2")
+    # def testChooseGameHumanVsHuman(self, mock_stdin):
+    #     # want to write a test that checks that players are objects of the Player class
+    #     # result = type(---._player1) // Cannot test local variable
+    #     # expected_result = Player
         
-        result = self.start_game.number_of_players(mock_stdin.return_value)
-        expected_result = "Human v. Human game starting.."
-        self.assertEqual(result, expected_result, msg='\nRetrieved:\n{0} \nExpected:\n{1}'.format(result, expected_result))
+    #     result = self.start_game.number_of_players(mock_stdin.return_value)
+    #     expected_result = "Human v. Human game starting.."
+    #     self.assertEqual(result, expected_result, msg='\nRetrieved:\n{0} \nExpected:\n{1}'.format(result, expected_result))
         
--- tictactoe/ai_player.py
+++ tictactoe/ai_player.py
@@ -1,6 +1,5 @@
 class AIPlayer:
-    def __init__(self, symbol, input_getter):
-        self._input = input_getter
+    def __init__(self, symbol):
         self._symbol = symbol
 
     def move(self):
