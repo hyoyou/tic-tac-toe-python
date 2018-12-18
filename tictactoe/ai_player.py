25e4e93930c7c7f67d926edd23952c67a9b3f727
Heather You <heather.fswd@gmail.com>
2018-12-18 14:22:56 -0500

    Remove WINNING_COMBOS constant from class and use constant from Game class

--- tictactoe/ai_player.py
+++ tictactoe/ai_player.py
@@ -1,17 +1,7 @@
 import random
+from tictactoe.game import Game
 
 class AIPlayer:
-    WINNING_COMBOS = [
-        [0, 1, 2], # Top row
-        [3, 4, 5], # Middle row
-        [6, 7, 8], # Bottom row
-        [0, 3, 6], # Left column
-        [1, 4, 7], # Middle column
-        [2, 5, 8], # Right column
-        [0, 4, 8], # Left diagonal
-        [2, 4, 6]  # Right diagonal
-    ]
-
     def __init__(self, symbol):
         self._symbol = symbol
 
@@ -21,22 +11,18 @@ class AIPlayer:
         sym = self._symbol
         opp = "O" if sym == "X" else "X"
 
-        # Try to take the center
         if board.turn_count() == 0 and board.is_valid_move(5):
             best_move = 5
         elif board.turn_count() == 1 and board.is_valid_move(5):
             best_move = 5
 
-        # Try to take the corner
         if board.turn_count() == 1 and board.is_valid_move(3) and not board.is_valid_move(5):
             best_move = 3
         elif board.turn_count() >= 2 and board.is_valid_move(3):
             best_move = 3
 
-        # Best moves after first 2 moves made:
-        # Move for a block
         if board.turn_count() >= 2:
-            for combo in self.WINNING_COMBOS:
+            for combo in Game.WINNING_COMBOS:
                 if (board._board[combo[1]] == opp and 
                     board._board[combo[2]] == opp and
                     board._board[combo[0]] == " "):
@@ -50,9 +36,8 @@ class AIPlayer:
                     board._board[combo[2]] == " "):
                     best_move = combo[2] + 1
 
-        # Move for a win
         if board.turn_count() >= 2:
-            for combo in self.WINNING_COMBOS:
+            for combo in Game.WINNING_COMBOS:
                 if (board._board[combo[1]] == sym and 
                     board._board[combo[2]] == sym and
                     board._board[combo[0]] == " "):
