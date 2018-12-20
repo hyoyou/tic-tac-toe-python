554bb0746ec4f7cd18dd47633d36582bcf904b5d
Heather You <heather.fswd@gmail.com>
2018-12-20 17:46:27 -0500

    Refactor conditionals into new methods

--- tictactoe/ai_player.py
+++ tictactoe/ai_player.py
@@ -12,41 +12,50 @@ class AIPlayer:
         sym = self._symbol
         opp = "O" if sym == "X" else "X"
 
-        if validator.is_empty(5, board):
+        if self.middle_cell_is_available(validator, board):
             return 5
 
-        if validator.is_empty(3, board):
+        if self.corner_cell_is_available(validator, board):
             return 3
    
-        if self.make_move(board, sym) != None:
+        if self.winning_move_is_available(board, sym):
             return self.make_move(board, sym)
-        elif self.make_move(board, opp) != None:
+        elif self.opponent_block_is_needed(board, opp):
             return self.make_move(board, opp)
         
         return self.random_move(board, validator)
         
-    def make_move(self, board, sym):
-        spaces = board.spaces()
+    def middle_cell_is_available(self, validator, board):
+        return validator.is_empty(5, board)
+    
+    def corner_cell_is_available(self, validator, board):
+        return validator.is_empty(3, board)
+    
+    def winning_move_is_available(self, board, symbol):
+        return self.make_move(board, symbol) != None
+    
+    def opponent_block_is_needed(self, board, symbol):
+        return self.make_move(board, symbol) != None
 
+    def make_move(self, board, symbol):
+        spaces = board.spaces()
         for combo in Game.WINNING_COMBOS:
-            if (spaces[combo[1]] == sym and 
-                spaces[combo[2]] == sym and
+            if (spaces[combo[1]] == symbol and 
+                spaces[combo[2]] == symbol and
                 spaces[combo[0]] == " "):
                 return combo[0] + 1
-            elif (spaces[combo[0]] == sym and 
-                spaces[combo[2]] == sym and
+            elif (spaces[combo[0]] == symbol and 
+                spaces[combo[2]] == symbol and
                 spaces[combo[1]] == " "):
                 return combo[1] + 1
-            elif (spaces[combo[0]] == sym and 
-                spaces[combo[1]] == sym and
+            elif (spaces[combo[0]] == symbol and 
+                spaces[combo[1]] == symbol and
                 spaces[combo[2]] == " "):
                 return combo[2] + 1
-        
         return None
 
     def random_move(self, board, validator):
         best_move = 5
         while not validator.is_empty(best_move, board):
             best_move = random.randint(1, 9)
-
         return best_move
