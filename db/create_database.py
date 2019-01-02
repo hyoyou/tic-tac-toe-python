3b5baadb23e5f17883d3631e18a0f336f6f73f02
Heather You <heather.fswd@gmail.com>
2019-01-02 16:50:23 -0500

    Set up database
    WIP
    
    WIP

---
+++ db/create_database.py
@@ -0,0 +1,20 @@
+from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql import *
+from db import engine
+from .mutable_wrapper import MutableTypeWrapper
+import datetime
+
+Base = declarative_base()
+
+class Game(Base):
+    __tablename__ = "games"
+
+    id = Column(Integer, primary_key=True)
+    game = Column(PickleType)
+    timestamp = Column(DateTime, default=datetime.datetime.now)
+
+    def __repr__(self):
+        return "<Game(id='%s', game='%s', timestamp='%s')>" % (self.id, self.game, self.timestamp)
+
+Game.__table__.create(bind=engine, checkfirst=True)
---
+++ db/database_functions.py
@@ -0,0 +1,38 @@
+from sqlalchemy import MetaData, Table, desc
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import select
+from sqlalchemy.sql.expression import exists
+from .create_database import Game
+import code
+import pickle
+from db import engine
+
+meta = MetaData(bind=engine)
+game = Table("games", meta, autoload=True, autoload_with=engine)
+
+def create_session():
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return session
+
+def retrieve_last_game(session):
+    engine.connect()
+    game_id, pickled_game, date = session.query(game).order_by(desc(Game.id)).first()
+    game_obj = pickle.loads(pickled_game)
+    return game_obj
+
+def add_game_to_database(game_object, session):
+    engine.connect()
+    current_game = Game(game=game_object)
+    session.add(current_game)
+    session.commit()
+    session.close()
+
+def delete_game_from_database(session):
+    engine.connect()
+    if session.query(game).order_by(desc(Game.id)).first():
+        completed_game_id, pickled_game, date = session.query(game).order_by(desc(Game.id)).first()
+        completed_game_entry = session.query(Game).filter_by(id = completed_game_id).one()
+        session.delete(completed_game_entry)
+        session.commit()
+        session.close()
