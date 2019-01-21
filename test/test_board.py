158a1cf13632d2fcd95fd08533a79163830fb1b9
Jason <jgoodman@8thlight.com>
2019-01-21 18:12:55 -0500

    Remove Player, MockCLIOutput, and Rules from Board unit tests

--- test/test_board.py
+++ test/test_board.py
@@ -1,31 +1,24 @@
 import unittest
-from .mock_cli_input import MockCLIInput
-from .mock_cli_output import MockCLIOutput
+from tictactoe.constants import X, O, EMPTY_SPACE
 from tictactoe.board import Board
-from tictactoe.player import Player
-from tictactoe.rules import Rules
 
 class BoardTest(unittest.TestCase):
     def setUp(self):
         self.board = Board()
-        self.output = MockCLIOutput()
-        self.player1 = Player("X", MockCLIInput(), self.output)
-        self.player2 = Player("O", MockCLIInput(), self.output)
-        self.rules = Rules()
 
     def testBoardExistsAndIsAnEmptyListWith9Spaces(self):
         result = self.board._board
-        expected_result = [" " for i in range(9)]
+        expected_result = [EMPTY_SPACE for i in range(9)]
         self.assertEqual(result, expected_result, msg='\nRetrieved:\n{0} \nExpected:\n{1}'.format(result, expected_result))
 
     def testBoardReturnsSpacesAsAnEmptyListWithoutMove(self):
         result = self.board.spaces()
-        expected_result = [" " for i in range(9)]
+        expected_result = [EMPTY_SPACE for i in range(9)]
         self.assertEqual(result, expected_result, msg='\nRetrieved:\n{0} \nExpected:\n{1}'.format(result, expected_result))
 
     def testBoardReturnsCorrectSpacesAsListWithMoves(self):
-        self.board.make_move(1, self.player1._symbol)
-        self.board.make_move(5, self.player2._symbol)
+        self.board.make_move(1, X)
+        self.board.make_move(5, O)
 
         result = self.board.spaces()
         expected_result = ['X', ' ', ' ', ' ', 'O', ' ', ' ', ' ', ' ']
@@ -37,34 +30,34 @@ class BoardTest(unittest.TestCase):
         self.assertEqual(result, expected_result, msg='\nRetrieved:\n{0} \nExpected:\n{1}'.format(result, expected_result))
 
     def testBoardReturnsAListOfEmptyCellsWhenEmptyCellsCalled(self):
-        self.board.make_move(1, self.player1._symbol)
-        self.board.make_move(2, self.player2._symbol)
-        self.board.make_move(3, self.player1._symbol)
-        self.board.make_move(4, self.player2._symbol)
-        self.board.make_move(5, self.player1._symbol)
+        self.board.make_move(1, X)
+        self.board.make_move(2, O)
+        self.board.make_move(3, X)
+        self.board.make_move(4, O)
+        self.board.make_move(5, X)
 
         result = self.board.empty_cells()
         expected_result = [5, 6, 7, 8]
         self.assertEqual(result, expected_result, msg='\nRetrieved:\n{0} \nExpected:\n{1}'.format(result, expected_result))
 
     def testBoardReturnsThePlayerSymbolWhenSpaceCalledOnCellThatHasBeenPlayed(self):
-        self.board.make_move(5, self.player1._symbol)
+        self.board.make_move(5, X)
 
         result = self.board.space(5)
         expected_result = "X"
         self.assertEqual(result, expected_result, msg='\nRetrieved:\n{0} \nExpected:\n{1}'.format(result, expected_result))
 
     def testBoardUpdatesWhenPlayerMakesMove(self):
-        self.board.make_move(5, self.player1._symbol)
+        self.board.make_move(5, X)
 
         result = self.board.spaces()
         expected_result = [' ', ' ', ' ', ' ', 'X', ' ', ' ', ' ', ' ']
         self.assertEqual(result, expected_result, msg='\nRetrieved:\n{0} \nExpected:\n{1}'.format(result, expected_result))
 
     def testBoardReturnsTurnCountBasedOnNumberOfAvailableSpacesOnBoard(self):
-        self.board.make_move(1, self.player1._symbol)
-        self.board.make_move(3, self.player2._symbol)
-        self.board.make_move(5, self.player1._symbol)
+        self.board.make_move(1, X)
+        self.board.make_move(3, O)
+        self.board.make_move(5, X)
 
         result = self.board.turn_count()
         expected_result = 3
