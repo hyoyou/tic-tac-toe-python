45acc1ed0b6b034b8c61bf76def4c40a604caa49
Jason <jgoodman@8thlight.com>
2019-01-21 18:15:16 -0500

    Use EMPTY_SPACE constant in Board class

--- tictactoe/board.py
+++ tictactoe/board.py
@@ -1,20 +1,20 @@
-from .constants import WINNING_COMBOS
+from .constants import WINNING_COMBOS, EMPTY_SPACE
 
 class Board:
     def __init__(self, board=None):
-        self._board = [" " for i in range(9)] if board == None else board
+        self._board = [EMPTY_SPACE for i in range(9)] if board == None else board
 
     def spaces(self):
         return self._board
 
     def space(self, cell):
-        return self._board[cell-1] if self._board[cell-1] != " " else cell
+        return self._board[cell-1] if self._board[cell-1] != EMPTY_SPACE else cell
 
     def empty_cells(self):
-        return [i for i, e in enumerate(self._board) if e == " "]
+        return [i for i, e in enumerate(self._board) if e == EMPTY_SPACE]
 
     def make_move(self, cell, player_symbol):
         self._board[cell-1] = player_symbol
 
     def turn_count(self):
-        return 9 - self._board.count(" ")
+        return 9 - self._board.count(EMPTY_SPACE)
